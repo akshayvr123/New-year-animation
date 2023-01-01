78fcd1e9ae222008e475f37595882a3e53894972
akshayvr123 <102403623+akshayvr123@users.noreply.github.com>
2023-01-01 18:50:02 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,15 @@
+var a=document.getElementById("input")
+var b=document.getElementById("name")
+
+
+
+function message(){
+    if(a.value===""){
+        alert("Please Enter your name")
+    }else{
+
+ document.getElementById("welcome").style.display = "block";
+     
+    b.innerHTML="Happy New Year "+a.value
+}
+}
